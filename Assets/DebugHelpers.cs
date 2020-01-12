e074c2971fc17ab5324c4b9376701575be2e6bbd
Noah Stolmaker <nstolmaker@gmail.com>
2020-01-11 19:12:28 -0800

    first commit. still just fiddline around and nothing works

---
+++ Assets/DebugHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugHelpers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Oculus_CrossPlatform_PrimaryThumbstick") || Input.GetKeyDown(KeyCode.Tilde)) {
+            Debug.Log("Primary thumbstick pressed");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Debug.Log("tab pressed");
+            GameObject.Find("RedButton").GetComponentInChildren<PoolTableButtons>().ButtonGoesIn();
+        }
+        
+    }
+}
---
+++ Assets/Puck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puck : MonoBehaviour
+{
+
+    public float strikeForce = 2.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            Vector3 direction = input.normalized;
+            Vector3 velocity = direction * strikeForce;
+            this.GetComponentInParent<Rigidbody>().AddForce(velocity, ForceMode.Acceleration);
+
+
+
+
+    }
+}
