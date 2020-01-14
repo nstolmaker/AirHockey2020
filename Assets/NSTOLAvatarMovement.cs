9fe718bd14c32a3f279a454031ff30f2746aed39
Noah Stolmaker <nstolmaker@gmail.com>
2020-01-14 00:10:22 -0800

    ripped out VRTK and am using the Unity Built-in XR stuff mostly now. From the tutorial on raywenderlich.com - currently I managed to get it to turn on, and you can grab a paddle, and if you manage to hit the puck into the goal it counts. You can also walk around with the joystick. Problems remaining that I can think of: You cannot let go of the Hockey Pad once you pick it up, and it rotates around all crazily in your hand. Movement only works as long as you keep facing the same direction. It needs to re-trigger the y axis centering when you turn your head. Definitely need to fix the physics of the table, puck, and pads. Hitting the reset buttons doesnt seem to work. The scale is all wonky and too big/small. And, I havent even tried to hook normcore in yet. It might just work better because its using less prefabs with dependencies, but I failed to get Aliases working after trying really hard for some hours, so if I cant wire Normcore in directly, its gonna be a headache getting it to work

--- Assets/NSTOLAvatarMovement.cs
+++ Assets/NSTOLAvatarMovement.cs
@@ -9,7 +9,8 @@ public class NSTOLAvatarMovement : MonoBehaviour
     public Transform avatar;
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
-    private string hand = "left";
+    [Tooltip("Valid options are left and right. Case sensitive.")]
+    public string hand;
 
     void Start()
     {
@@ -19,29 +20,34 @@ public class NSTOLAvatarMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 ovrinput = new Vector2(0,0);
+        Vector2 ovrinput = new Vector2(); // = new Vector2(0,0);
         // assume it's an oculus controller
         //float translation = Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickHorizontal") * speed;
         switch (hand)
         {
             case "right":
-                ovrinput = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad, OVRInput.Controller.RTrackedRemote);
+                ovrinput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.RTouch);
                 break;
             case "left":
-                ovrinput = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad, OVRInput.Controller.LTrackedRemote);
+                ovrinput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.LTouch);
                 break;
         }
-        
-        //float rotation = Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical") * rotationSpeed;
-        Debug.Log("got x and y axis for controller: [" + ovrinput.ToString() + "]");
+
+
 
         // Make it move 10 meters per second instead of 10 meters per frame...
-        Vector2 scaledOvrinput = ovrinput * Time.deltaTime;
-        Vector3 movementAmount = new Vector3(scaledOvrinput.x, scaledOvrinput.y);
+        Vector2 scaledOvrinput = ovrinput * speed * Time.deltaTime;
+        float xAxis = scaledOvrinput.x;
+        float yAxis = scaledOvrinput.y;
+        Vector3 movementAmount = new Vector3(0f, xAxis, yAxis);
         //rotation *= Time.deltaTime;
-
+        //float rotation = Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical") * rotationSpeed;
+        if (ovrinput != new Vector2() && ovrinput != new Vector2())
+        {
+            Debug.Log("got x and y axis for " + hand + " hand controller: " + movementAmount.ToString());
+        }
         // Move translation along the object's z-axis
-        transform.Translate(scaledOvrinput);
-
+        avatar.transform.Translate(xAxis, 0f, yAxis);
+        ovrinput = new Vector2();
     }
 }
---
+++ Assets/NSTOLPersonPhysics.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NSTOLPersonPhysics : MonoBehaviour
+{
+
+
+    // The purpose of this is to take some tips from VRTK's BodyPhysics and implement it. Simple stuff like the person's body colliding with things. 
+    private float personVerticalStartingPosition;
+    private float allowedYThreshold = 1f; // how far are they allowed to move their head vertically (both up and down) before we reset it. In meters.
+    [Tooltip("This limits squating and jumping, but prevents people from going flying over into the void and other weird stuff that can happen.")]
+    public bool limitVerticalMovement = true;
+    [Tooltip("VALUE VISIBLE FOR DEBUGGING ONYL: Should be set automatically. If it isn't, make sure you have a OVRCameraRig as a child of this avatar game object")]
+    public OVRCameraRig VRRig;
+    [Tooltip("VALUE VISIBLE FOR DEBUGGING ONYL: Should be set automatically. If it isn't, make sure you have a OVRCameraRig as a child of this avatar game object. That object should have a TrackingSpace Gameobject with a child CenterEyeAnchor camera")]
+    public Transform personsEyes;
+
+
+
+    private void Start()
+    {
+        VRRig = GetComponentInChildren<OVRCameraRig>();
+        personsEyes = VRRig.transform.Find("TrackingSpace").transform.Find("CenterEyeAnchor");
+        this.personVerticalStartingPosition = personsEyes.transform.position.y;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.limitVerticalMovement)
+        {
+            //this.limitHeadPosition();
+        }
+    }
+
+    public virtual void isStanding()
+    {
+
+    }
+
+    private void limitHeadPosition()
+    {
+        if (this.limitVerticalMovement)
+        {
+            float personVerticalPositionRightNow = personsEyes.position.y;   // y position of their head, right now.
+            Vector2 personGroundPositionRightNow = new Vector3(personsEyes.position.x, personsEyes.position.z, 0f);
+            float personVerticalChangeAmount = Mathf.Abs(this.personVerticalStartingPosition - personVerticalPositionRightNow); // how much their head has moved
+            float personPositionMinVerticalPosition = personVerticalStartingPosition - this.allowedYThreshold;  // how low can you go?
+            float personPositionMaxVerticalPosition = personVerticalStartingPosition + this.allowedYThreshold;  // how high can you fly?
+            if (personVerticalChangeAmount > this.allowedYThreshold) // if they move their head vertically more than I allow, clamp their position.
+            {
+                // forcefully sets the position based on their current position, and the limited vertical variance allowed by the threshold value.
+                /* 
+                 * transform.position = new Vector3(personGroundPositionRightNow.x,
+                    Mathf.Clamp(personVerticalPositionRightNow, personPositionMinVerticalPosition, personPositionMaxVerticalPosition),
+                    personGroundPositionRightNow.y); 
+                    */
+                transform.position = new Vector3(5,0,0);
+            }
+        }
+    } 
+}
